6a2671be2ab9fd92315f4bac991985c219668380
Krishan <xp3cto95@gmail.com>
2023-12-17 17:31:46 +0530

    Upload File Image Using Spring Boot

---
+++ uploadImage/src/main/java/lk/linex/uploadImage/ImgUploadController.java
@@ -0,0 +1,27 @@
+package lk.linex.uploadImage;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+
+@RequestMapping("api/v1/img")
+@RestController
+public class ImgUploadController {
+    @PostMapping("/upload")
+    public void saveImage(@RequestParam("files")MultipartFile[] files){
+        String uploadDir = "profileImagesFolder";
+        Arrays.asList(files).stream().forEach(file ->{
+            String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+            System.out.println(fileName);
+            try{
+                fileUploadUtil.saveFile(uploadDir,fileName,file);
+            }catch (IOException ioException){
+                ioException.printStackTrace();
+            }
+        });
+    }
+}
